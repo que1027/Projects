3d90967c39861115ef350a491c9973dc37cdc818
Quinton Taylor <quintontaylor29@gmail.com>
2025-05-12 11:46:20 -0500

    finished palindrome checker

--- PalendromeValidator.cpp
+++ PalendromeValidator.cpp
@@ -3,13 +3,26 @@
 
 int main(){
     std::string word;
+    std::cout << "\n";
+    std::cout << "Please enter a word and I will tell you if it is a palindrome \n";
     std::cin >> word;
     std::vector<char> arr(word.begin(), word.end());
-    i =word.length();
-    for(char c : arr){
-        for(i;i>=1;i--){
-
-}
-}
+    int i = 0;
+    int j = word.length() - 1;;
+    bool isPalindrome = true;
+    while(i < j){
+       
+        if(arr[i] != arr[j]){
+            isPalindrome = false;
+            std::cout << word << " is not a palindrome";
+            break;
+        }else{
+            isPalindrome = true;
+            std::cout << word << " is a palindrome";
+            break;
+        }
+        i++;
+        j--;
+    }
 }
 
